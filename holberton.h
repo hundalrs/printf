5c828eca80ebeca1503c2132078a8b77e77955f6
Raman Hundal <rshundal11@gmail.com>
2017-10-23 03:10:09 +0000

    Added prototypes to header file

--- holberton.h
+++ holberton.h
@@ -2,14 +2,17 @@
 #define HOLBERTON
 
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-
-typedef struct format
+typedef struct form
 {
-	char *format;
-	int (*f)(va_list));
+	char *mod;
+	int (*f)(va_list);
 } form_spec;
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+void print(int n);
 
 #endif
