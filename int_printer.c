98c56ccc7da85643d59cc6f648f1685f49f1f2ca
Raman Hundal <rshundal11@gmail.com>
2017-10-24 18:13:29 +0000

    Used while loops in function instead of recursion

--- int_printer.c
+++ int_printer.c
@@ -7,26 +7,39 @@
 */
 int helper_print_int (int n)
 {
-	int sum = 0;
+	int div, i, k, counter, x, sum = 0;
+	unsigned int num, num2;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
+	div = 1;
+	counter = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = n * -1;
+		sum++;
 	}
-
-	if (n / 10)
+	else
+		num = n;
+	num2 = num;
+	while (num2 > 0)
 	{
-		helper_print_int(n / 10);
+		num2 = num2 / 10;
+		counter++;
 	}
-
-	_putchar(n % 10 + '0');
+	for (i = 0; i < counter - 1; i++)
+	{
+		div = div * 10;
+	}
+	for (k = 0; k < counter - 1; k++)
+	{
+		x = num / div;
+		_putchar(x + '0');
+		sum++;
+		num = num % div;
+		div = div / 10;
+	}
+	_putchar(num % 10 + '0');
 	sum++;
 	return (sum);
 }
